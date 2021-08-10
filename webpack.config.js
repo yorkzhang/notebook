05bd00654472fb089eea95633302e92127f72d11
Zachary Sailer <zachsailer@gmail.com>
2021-08-10 15:54:36 -0700

    Revert "Switch webpack to production mode"

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['babel-polyfill', '@jupyterlab/apputils/lib/sanitizer'],
+  entry: '@jupyterlab/apputils/lib/sanitizer',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'notebook/static/components/sanitizer'),
-    libraryTarget: "amd",
-  },
-  devtool: false,
-  optimization: {
-    minimize: false
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?jsx?$/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          }
-        }
-      }
-    ]
+    libraryTarget: "amd"
   }
 }
